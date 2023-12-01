3a75b1151773dccbe71f138ab2097c025b83f404
Prajeesh <prajeeshrajan07@gmail.com>
2023-12-01 15:29:22 +0530

    Checkbox done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
   return (
     <div className="app">
       <div className="mainHeading">
@@ -8,22 +11,31 @@ function App() {
       </div>
       <div className="subHeading">
         <br />
-        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+        <h2>Whoop,<span>   .</span> Let's start ❤️</h2>
       </div>
       <div className="input">
-        <input type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+        <input type="text" value={toDo} onChange={(e) => { setToDo(e.target.value) }} placeholder="🙂Add item..." />
+        <i onClick={() => { setToDos([...toDos,{id:Date.now(), text:toDo, status:false}]) }} className="fas fa-plus"></i>
       </div>
       <div className="todos">
-        <div className="todo">
-          <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>Rect tutorial</p>
-          </div>
-          <div className="right">
-            <i className="fas fa-times"></i>
-          </div>
-        </div>
+        {  toDos.map((obj) => {
+
+
+          return (
+            <div className="todo">
+              <div className="left">
+                <input onChange={(e)=>{
+                  console.log(e.target.value)
+                  console.log(obj)
+                }}value={obj.status}type="checkbox" name="" id="" />
+                <p>{obj.text}</p>
+              </div>
+              <div className="right">
+                <i className="fas fa-times"></i>
+              </div>
+            </div>)
+        })
+        }
       </div>
     </div>
   );
